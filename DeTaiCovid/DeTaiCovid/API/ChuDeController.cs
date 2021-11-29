952aed880e176e5b5c28e15c15b1bc1d5040e927
sonloichau <83951302+sonloichau@users.noreply.github.com>
2021-11-29 23:54:35 +0700

    soncomid

---
+++ DeTaiCovid/DeTaiCovid/API/ChuDeController.cs
@@ -0,0 +1,74 @@
+﻿using DeTaiCovid.Controllers;
+using DeTaiCovid.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeTaiCovid.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChuDeController : ControllerBase
+    {
+        private ChuDeService ChuDeService = new ChuDeService();
+        [HttpGet]
+        public IActionResult LayDSChuDe()
+        {
+            var lstChuDe = ChuDeService.LayDSChuDe();
+            if (lstChuDe.Count > 0)
+            {
+
+                return Ok(lstChuDe);
+            }
+            else
+            {
+                return BadRequest("Danh Sách trống!");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult ThemChuDe(ChuDe newChuDe)
+        {
+            bool check = ChuDeService.ThemChuDe(newChuDe);
+            if (check == true)
+            {
+                return Ok($"Thêm chủ đề{newChuDe.ChuDeid} thành công");
+            }
+            else
+            {
+                return BadRequest($"Thêm chủ đề {newChuDe.ChuDeid} Thất bại");
+            }
+        }
+        [HttpPut]
+        public IActionResult SuaBaiViet(ChuDe chuDe)
+        {
+            bool check = ChuDeService.SuaChuDe(chuDe);
+            if (check == true)
+            {
+                return Ok($"sửa chủ đề  {chuDe.ChuDeid}   thành công");
+            }
+            else
+            {
+                return BadRequest($"Sửa  chủ đề  {chuDe.ChuDeid} Thất bại");
+
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult XoaChuDe(int ChuDeId)
+        {
+            bool check = ChuDeService.XoaChuDe(ChuDeId);
+            if (check == true)
+            {
+                return Ok($"xóa chủ đề {ChuDeId}  thành công");
+            }
+            else
+            {
+                return BadRequest($"xóa chủ đề  {ChuDeId}  Thất bại");
+            }
+        }
+    }
+}
---
+++ DeTaiCovid/DeTaiCovid/Controllers/ChuDeService.cs
@@ -0,0 +1,79 @@
+﻿using DeTaiCovid.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeTaiCovid.Controllers
+{
+    public class ChuDeService
+    {
+        private TinTucCovidContext DbContext = new TinTucCovidContext();
+
+        public List<ChuDe> LayDSChuDe()
+        {
+            return DbContext.ChuDes.ToList();
+        }
+        
+        public bool ThemChuDe(ChuDe newChuDe)
+        {
+            ChuDe currentChuDe = DbContext.ChuDes.SingleOrDefault(x => x.ChuDeid == newChuDe.ChuDeid);
+            ChuDe tenChuDe = DbContext.ChuDes.FirstOrDefault(x => x.TenChuDe == newChuDe.TenChuDe);
+
+            if (currentChuDe == null && tenChuDe == null)
+            {
+                DbContext.ChuDes.Add(newChuDe);
+                DbContext.SaveChanges();
+                return true;
+               
+                
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool SuaChuDe(ChuDe chuDe)
+        {
+            ChuDe currentChuDe = DbContext.ChuDes.SingleOrDefault(x => x.ChuDeid == chuDe.ChuDeid);         
+            if (currentChuDe == null)
+            {
+                return false;
+            }
+            else
+            {
+                currentChuDe.TenChuDe = chuDe.TenChuDe;              
+                DbContext.ChuDes.Update(currentChuDe);
+                DbContext.SaveChanges();
+                return true;
+            }
+        }
+
+        public bool XoaChuDe(int ChuDeId)
+        {
+            ChuDe currentChuDe = DbContext.ChuDes.SingleOrDefault(x => x.ChuDeid == ChuDeId);
+            List<BaiViet> lstBaiViet = DbContext.BaiViets.Where(x => x.ChuDeid == ChuDeId).ToList();//tim danh sach bai viet lien quan toi chu de do
+            if (currentChuDe == null)
+            {
+                return false;
+            }
+            else if (lstBaiViet.Count>0)//neu lst bai viết co du lieu thi th xoa
+            {
+                for (int i = 0; i < lstBaiViet.Count; i++)
+                {
+                    if (lstBaiViet[i].ChuDeid == ChuDeId)
+                    {
+                        DbContext.BaiViets.Remove(lstBaiViet[i]);
+                        DbContext.SaveChanges();
+                    }
+                }
+            }
+            //Xoa du lieu
+            DbContext.ChuDes.Remove(currentChuDe);
+            DbContext.SaveChanges();
+            return true;
+        }
+
+    }
+}
